67633e38d68b7fafb7477fce62a973dc015a6de3
trieuthang1991 <trieuthangtin18a1@gmail.com>
2017-11-21 11:14:53 +0700

    Lesson11 : Routing

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import {Routes, RouterModule} from '@angular/router';
 
+const UserRoutes : Routes = [
+  {path :'', redirectTo:'index',pathMatch:'full'},
+  {path:'index', component:UserComponent}
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(UserRoutes)
   ],
   declarations: [UserComponent]
 })
