38d97fc7e4a51d13b801bf0d31ed88c3d84319db
trieuthang1991 <trieuthangtin18a1@gmail.com>
2017-11-21 16:11:39 +0700

    lesson13:Main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewChecked,ElementRef } from '@angular/core';// Có thể dùng AfterContentInit cho AfterViewChecked
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewChecked{
+  constructor(private elementRef:ElementRef)
+  {
+
+  }
+  ngAfterViewChecked(){
+    var s = document.createElement("script");
+    s.type ="text/javascript";
+    s.src="../assets/Js/custom.js";
+    this.elementRef.nativeElement.appendChild(s);// Chú ý còn xóa trước khi dán
+   
+  }
 }
--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { Routes , RouterModule } from '@angular/router';
+
+
+
+const homeRoutes : Routes =
+[
+  {path: '',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:HomeComponent}
+
+]
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(homeRoutes)
+    
   ],
   declarations: [HomeComponent]
 })
