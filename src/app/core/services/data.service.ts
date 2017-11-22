2a8b7c14bcbb9db44d635e85beea1018338460b7
trieuthang1991 <trieuthangtin18a1@gmail.com>
2017-11-22 16:45:06 +0700

    Lesson15 : Service part 2

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,27 +1,67 @@
 import { Injectable } from '@angular/core';
-
+import {Http, Response,Headers} from '@angular/http';
+import {Router} from '@angular/router';
+import { SystemConstants  } from './../Common/system.constant';
+import {AuthenService} from './authen.service';
+import {Observable} from 'rxjs/Observable';
+import {MessageContants} from './../Common/message.constant';
+import {NotificationService} from './notification.service';
+import {UtilityService} from './utility.service'; 
 @Injectable()
 export class DataService {
+  private headers : Headers
 
-  constructor() { }
-  get(url:string){
-    return null;
+  constructor(private _http:Http , private _router:Router,private _authenService:AuthenService , 
+    private _notificationService:NotificationService,private _UtilityService:UtilityService){ }
+  get(uri:string){
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization","Bearer" + this._authenService.getLoggedInUser().access_token)
+    return this._http.get(SystemConstants.BASE_API + uri,{headers:this.headers}).map(this.extractData);
   }
-  post(url:string,data?:any)
+  post(uri:string,data?:any)
   {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization","Bearer" + this._authenService.getLoggedInUser().access_token)
+    return this._http.post(SystemConstants.BASE_API + uri,data,{headers:this.headers}).map(this.extractData);
   }
-  put(url:string ,data?:any)
+  put(uri:string ,data?:any)
   {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization","Bearer" + this._authenService.getLoggedInUser().access_token)
+    return this._http.put(SystemConstants.BASE_API + uri,data,{headers:this.headers}).map(this.extractData);
   }
-  delete(url:string,key:string,id:string)
+  delete(uri:string,key:string,id:string)
   {
-    return null;
+    this.headers.delete("Authorization");
+    this.headers.append("Authorization","Bearer" + this._authenService.getLoggedInUser().access_token)
+    return this._http.put(SystemConstants.BASE_API + uri + "/?"+ key +"=" +id,{headers:this.headers}).map(this.extractData);
   }
-  postFile(url:string,data?:any)
+  postFile(uri:string,data?:any)
   {
-    
+    let newHeader = new Headers();
+  
+    newHeader.append("Authorization","Bearer" + this._authenService.getLoggedInUser().access_token)
+    return this._http.put(SystemConstants.BASE_API + uri,data,{headers:newHeader}).map(this.extractData);
+  }
+  private extractData(res:Response)
+  {
+    let body=res.json();
+    return body || {};
+  }
+  public handleError(error:any)
+  {
+    if(error.status==401)
+    {
+      localStorage.removeItem(SystemConstants.Current_User);
+      this._notificationService.printErrorMessage(MessageContants.SYSTEM_ERROR_MSG);
+      this._UtilityService.navigateToLogin();
+    }
+    else
+    {
+      let errMsg = (error.message)? error.message: error.status?`${error.statustext}`:'Lỗi Hệ Thống';
+      this._notificationService.printErrorMessage(errMsg);
+      return Observable.throw(errMsg);
+    }
   }
 
 }
--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,8 +1,77 @@
 import { Injectable } from '@angular/core';
-
+declare var alertify: any;
 @Injectable()
 export class NotificationService {
+  private _nottifier : any =alertify;
+  constructor() {
+    alertify.defaults = {
+      // dialogs defaults
+      autoReset: true,
+      basic: false,
+      closable: true,
+      closableByDimmer: true,
+      frameless: false,
+      maintainFocus: true, // <== global default not per instance, applies to all dialogs
+      maximizable: true,
+      modal: true,
+      movable: true,
+      moveBounded: false,
+      overflow: true,
+      padding: true,
+      pinnable: true,
+      pinned: true,
+      preventBodyShift: false, // <== global default not per instance, applies to all dialogs
+      resizable: true,
+      startMaximized: false,
+      transition: 'pulse',
 
-  constructor() { }
+      // notifier defaults
+      notifier: {
+        // auto-dismiss wait time (in seconds)  
+        delay: 5,
+        // default position
+        position: 'bottom-right',
+        // adds a close button to notifier messages
+        closeButton: false
+      },
+
+      // language resources 
+      glossary: {
+        // dialogs default title
+        title: 'Xác Nhận',
+        // ok button text
+        ok: 'Đồng Ý',
+        // cancel button text
+        cancel: 'Hủy'
+      },
+
+      // theme settings
+      theme: {
+        // class name attached to prompt dialog input textbox.
+        input: 'ajs-input',
+        // class name attached to ok button
+        ok: 'ajs-ok',
+        // class name attached to cancel button 
+        cancel: 'ajs-cancel'
+      }
+    };
+  }
+  printSuccessMessage(message: string) {
+    
+        this._nottifier.success(message);
+      }
+    
+      printErrorMessage(message: string) {
+        this._nottifier.error(message);
+      }
+    
+      printConfirmationDialog(message: string, okCallback: () => any) {
+        this._nottifier.confirm(message, function (e) {
+          if (e) {
+            okCallback();
+          } else {
+          }
+        });
+      }
 
 }
