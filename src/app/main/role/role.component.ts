f3efa57e23d1a05eea479e9342204cf8d0d6765c
trieuthang1991 <trieuthangtin18a1@gmail.com>
2017-11-23 18:29:42 +0700

    Lesson22 : Update

--- src/app/main/role/role.component.ts
+++ src/app/main/role/role.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { ModalDirective } from 'ngx-bootstrap/modal';
-import {NotificationService} from '../../core/services/notification.service';
-import {MessageContants} from '../../core/Common/message.constant';
+import { NotificationService } from '../../core/services/notification.service';
+import { MessageContants } from '../../core/Common/message.constant';
 import { error } from 'util';
 @Component({
   selector: 'app-role',
@@ -10,14 +10,14 @@ import { error } from 'util';
   styleUrls: ['./role.component.css']
 })
 export class RoleComponent implements OnInit {
-  @ViewChild('modalAddEdit') public modalAddEdit:ModalDirective
+  @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective
   public pageIndex: number = 1;
   public pageSize: number = 20;
   public pageDisplay: number = 10;
-  public totalRow:number;
+  public totalRow: number;
   public filter: string = '';
   public roles: any[];
-  public entity:any;
+  public entity: any;
   constructor(private _dataService: DataService, private _notificationService: NotificationService) { }
 
   ngOnInit() {
@@ -27,39 +27,50 @@ export class RoleComponent implements OnInit {
   loadData() {
     this._dataService.get('/api/appRole/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
       .subscribe((response: any) => {
-      this.roles = response.Items;
-      console.log(response);
-      this.pageIndex=response.PageIndex;
-      this.pageSize = response.PageSize;
-      this.totalRow= response.TotalRows;
+        this.roles = response.Items;
+        
+        this.pageIndex = response.PageIndex;
+        this.pageSize = response.PageSize;
+        this.totalRow = response.TotalRows;
       });
   }
-  pageChanged(event:any):void
-  {
-   this.pageIndex=event.page;
-   this.loadData();
+  loadId(id:any) {
+    this._dataService.get('/api/appRole/detail/'+id)
+      .subscribe((response: any) => {
+       this.entity = response;
+       console.log(this.entity);
+      });
   }
-  showAddModal()
-  {
-    this.entity= {};
+  pageChanged(event: any): void {
+    this.pageIndex = event.page;
+    this.loadData();
+  }
+  showAddModal() {
+    this.entity = {};
     this.modalAddEdit.show();
   }
-  saveChange(valid:boolean)
+  ShowEditModal(id:any)
   {
-    if(valid)
-    {
-      if(this.entity.Id==undefined)
-      {
-        this._dataService.post('/api/appRole/add',JSON.stringify(this.entity)).subscribe((response:any)=>
-      {
-        this.loadData();
-        this.modalAddEdit.hide();
-        this._notificationService.printSuccessMessage(MessageContants.CREATED_OK_MSG);
-      },error=>this._dataService.handleError(error));
+    this.loadId(id);
+    this.modalAddEdit.show();
+  }
+  saveChange(valid: boolean) {
+    if (valid) {
+      if (this.entity.Id == undefined) {
+        this._dataService.post('/api/appRole/add', JSON.stringify(this.entity)).subscribe((response: any) => {
+          this.loadData();
+          this.modalAddEdit.hide();
+          this._notificationService.printSuccessMessage(MessageContants.CREATED_OK_MSG);
+        }, error => this._dataService.handleError(error));
       }
-      else
-      {
-
+      else {
+      
+          this._dataService.put('/api/appRole/update', JSON.stringify(this.entity)).subscribe((response: any) => {
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageContants.UPDATED_OK_MEG);
+          }, error => this._dataService.handleError(error));
+        
       }
     }
   }
