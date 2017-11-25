b97c8a520cdf75b52fb42db5f7e5c24beffc008e
trieuthang1991 <trieuthangtin18a1@gmail.com>
2017-11-25 09:48:08 +0700

    Lesson30 treeview

--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FunctionComponent } from './function.component';
+import { Routes,RouterModule } from '@angular/router';
+import { TreeModule } from 'angular-tree-component';
+import {FormsModule} from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap';
+const functionRoutes :Routes=[
+  {path :'', redirectTo:'index',pathMatch:'full'},
+  {path:'index' ,component:FunctionComponent}
+]
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TreeModule,
+    FormsModule,
+    ModalModule,
+    RouterModule.forChild(functionRoutes)
   ],
   declarations: [FunctionComponent]
 })
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -6,7 +6,8 @@ export const mainRoutes:Routes=[
             {path:'',redirectTo:"home",pathMatch:'full'},
             {path:'home',loadChildren:'./home/home.module#HomeModule'},
             {path:'user',loadChildren:'./user/user.module#UserModule'},
-            {path:'role',loadChildren:'./role/role.module#RoleModule'}
+            {path:'role',loadChildren:'./role/role.module#RoleModule'},
+            { path: 'function', loadChildren: './function/function.module#FunctionModule' }
         ]
     }
     
